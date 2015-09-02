9fd022ad0748d59bc5ce2887e4edf6f150023e9a
Brian Mitchell <brian.mitchell@imagineeasy.com>
2015-09-02 16:48:20 -0400

    Use callback definitions instead of behaviour_info/1

--- src/config_listener.erl
+++ src/config_listener.erl
@@ -19,43 +19,54 @@
 -export([start/2]).
 -export([start/3]).
 
--export([behaviour_info/1]).
-
 %% Required gen_event interface
--export([init/1, handle_event/2, handle_call/2, handle_info/2, terminate/2,
-    code_change/3]).
+-export([
+	init/1,
+	handle_event/2,
+	handle_call/2,
+	handle_info/2,
+	terminate/2,
+	code_change/3
+]).
 
-behaviour_info(callbacks) ->
-    [{handle_config_change,5},
-     {handle_config_terminate, 3}];
-behaviour_info(_) ->
-    undefined.
+%% config_listener behavior interface
+-callback handle_config_change(Section, Key, Value, Persist, State) -> {ok, NewState} | remove_handler when
+	Section :: string(),
+	Key :: string(),
+	Value :: term(),
+	Persist :: true | false,
+	State :: term(),
+	NewState :: term().
+-callback handle_config_terminate(Subscriber, Reason, State) -> any() when
+	Subscriber :: atom(),
+	Reason :: term(),
+	State :: term().
 
 start(Module, State) ->
-    start(Module, Module, State).
+	start(Module, Module, State).
 
 start(Module, Id, State) ->
-    gen_event:add_sup_handler(config_event, {?MODULE, Id}, {Module, State}).
+	gen_event:add_sup_handler(config_event, {?MODULE, Id}, {Module, State}).
 
 init({Module, State}) ->
-    {ok, {Module, State}}.
+	{ok, {Module, State}}.
 
 handle_event({config_change, Sec, Key, Value, Persist}, {Module, {From, State}}) ->
-    case Module:handle_config_change(Sec, Key, Value, Persist, State) of
-        {ok, NewState} ->
-            {ok, {Module, {From, NewState}}};
-        remove_handler ->
-            remove_handler
-    end.
+	case Module:handle_config_change(Sec, Key, Value, Persist, State) of
+		{ok, NewState} ->
+			{ok, {Module, {From, NewState}}};
+		remove_handler ->
+			remove_handler
+	end.
 
 handle_call(_Request, St) ->
-    {ok, ignored, St}.
+	{ok, ignored, St}.
 
 handle_info(_Info, St) ->
-    {ok, St}.
+	{ok, St}.
 
 terminate(Reason, {Module, {Subscriber, State}}) ->
-    Module:handle_config_terminate(Subscriber, Reason, State).
+	Module:handle_config_terminate(Subscriber, Reason, State).
 
 code_change(_OldVsn, St, _Extra) ->
-    {ok, St}.
+	{ok, St}.
